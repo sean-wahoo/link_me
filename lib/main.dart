df4c9e1f8b5e5c726ca85d2e98241b40f9ede49c
sean-wahoo <seanreichel.important@gmail.com>
2020-07-24 23:41:19 +0000

    created gradient for home page, added logo images

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,34 @@ void main() {
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    final Color topGreen = HexColor.fromHex('#4BDF33');
+    final Color midGreen = HexColor.fromHex('#33DF6F');
+    final Color bottomGreen = HexColor.fromHex('#47B06B');
     return MaterialApp(
-      title: "LinkMe",
-      theme: ThemeData(
-        primaryColor: (HexColor.fromHex('#000000')),
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Wowee app"),
-        ),
-        body: Center(
-          child: Text("yay"),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    topGreen,
+                    midGreen,
+                    bottomGreen,
+                  ],
+                  stops: [
+                    0.1,
+                    0.4,
+                    0.9,
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
