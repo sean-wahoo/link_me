7896fd0edb4a69918267bbecbf5665d7d82281a5
sean-wahoo <seanreichel.important@gmail.com>
2020-07-25 00:24:40 +0000

    added firebase dependencies, connected app to firebase, began work on authentication

---
+++ lib/services/auth.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+
+class AuthService {
+  // register with email/pass
+
+  // sign in with email/pass
+
+  // sign out with email/pass
+
+  // sign in with google
+
+}
